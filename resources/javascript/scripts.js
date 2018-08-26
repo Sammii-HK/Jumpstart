c896d36de20d240c4f7eb548ae7480c7c335a5a8
Sammii-HK <sammii.h@icloud.com>
2018-08-26 23:24:05 +0100

    image transition fix

---
+++ resources/javascript/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('.corousel').slick({
+	rtl: true,
+	infinite: true,
+	speed: 300,
+	slidesToShow: 1,
+	centerMode: true,
+	variableWidth: true,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 4000,
+	arrows: false,
+	draggable: false
+
+  });
+});
+
