bb32ded4b33bab5a1f917d6cc4fa707e1c3f0a93
Sammii-HK <sammii.h@icloud.com>
2018-08-28 21:23:39 +0100

    main.js fix

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -8,52 +8,7 @@ $(document).ready(function(){
     pauseOnFocus: false,
     pauseOnHover: false
   });
-})$(document).ready(function(){
-  $('.carousel').slick({
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    arrows: false,
-    draggable: false,
-    pauseOnFocus: false,
-    pauseOnHover: false
-  });
-})$(document).ready(function(){
-  $('.carousel').slick({
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    arrows: false,
-    draggable: false,
-    pauseOnFocus: false,
-    pauseOnHover: false
-  });
-})$(document).ready(function(){
-  $('.carousel').slick({
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    arrows: false,
-    draggable: false,
-    pauseOnFocus: false,
-    pauseOnHover: false
-  });
-})$(document).ready(function () {
-  $('.carousel').slick( {
-	rtl: true,
-	infinite: true,
-	speed: 300,
-	slidesToShow: 1,
-	centerMode: true,
-	variableWidth: true,
-	slidesToScroll: 1,
-	autoplay: true,
-	autoplaySpeed: 4000,
-	arrows: false,
-	draggable: false,
-
-  });
-});
+})
 
 var navScrollHide.scroll() {
 	.scrollTop(this)
