2067883f1706983e6a37c33e94b4689769e054fa
Sammii-HK <sammii.h@icloud.com>
2018-09-02 20:18:22 +0100

    nav bar color fix

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -8,6 +8,35 @@ $(document).ready(function(){
     pauseOnFocus: false,
     pauseOnHover: false
   });
-})
 
 
+/*
+	var scrollTop = 0;
+  $(window).scroll(function(){
+	var scrollValue = $(this).scrollTop();
+	if (scrollValue - scrollTop > 50){
+		var navDisappear = $('.navbar').css('height');
+		$('.navbar').animate({top: '-' + navDisappear}, 150);
+		scrollTop = scrollValue;
+	}   else if (scrollTop - scrollValue > 50) {
+		$('.navbar').animate({top: '0px'}, 150);
+		scrollTop = scrollValue;
+	}
+  });
+});  */
+	
+
+
+  var lastScrollTop = 0;
+  $(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - lastScrollTop > 50){
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      lastScrollTop = scrollTop;
+    } else if (lastScrollTop - scrollTop > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      lastScrollTop = scrollTop;
+    }
+  });
+}); 
